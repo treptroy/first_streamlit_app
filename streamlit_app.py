b21b2e1006f8ea7dee66a79d035f1713eceac2ad
treptroy <103455753+treptroy@users.noreply.github.com>
2022-04-11 17:04:05 -0400

    Update streamlit_app.py
    streamlit.header ('Breakfast Menu')
    streamlit.text ('omega 3 & blueberry oatmeal')
    streamlit.text ('kale, Spinach & Rocket smoothie')
    streamlit.text ('Hard-boiled Free Range egg')
    streamlit.header ('emojis Breakfast Menu')
    streamlit.text ('emojis omega 3 & blueberry oatmeal')
    streamlit.text ('emojis kale, Spinach & Rocket smoothie')
    streamlit.text ('emojis Hard-boiled Free Range egg')

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit
 
 streamlit.title('My mom\'s New Healthy Diner')
 
-streamlit.header ('Breakfast Menu')
-streamlit.text ('omega 3 & blueberry oatmeal')
-streamlit.text ('kale, Spinach & Rocket smoothie')
-streamlit.text ('Hard-boiled Free Range egg')
+streamlit.header ('emojis Breakfast Menu')
+streamlit.text ('emojis omega 3 & blueberry oatmeal')
+streamlit.text ('emojis kale, Spinach & Rocket smoothie')
+streamlit.text ('emojis Hard-boiled Free Range egg')
