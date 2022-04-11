d21a0138981c10f4413703f4b45d0f476bcd9f9c
treptroy <103455753+treptroy@users.noreply.github.com>
2022-04-11 19:19:39 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,3 +16,5 @@ streamlit.text(':apple:')
 import pandas
 
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+
+streamlit.dataframe(my_fruit_list)
