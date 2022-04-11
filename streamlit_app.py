7c58922ec24d2d27792907d94570ca52e234021c
treptroy <103455753+treptroy@users.noreply.github.com>
2022-04-11 18:31:33 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,10 +5,11 @@ streamlit.title('My mom\'s New Healthy Diner')
 streamlit.header ('Breakfast Favorites')
 streamlit.text ('🥣omega 3 & blueberry oatmeal')
 streamlit.text ('🥗kale kale, Spinach & Rocket smoothie')
-streamlit.text (':mango: Hard-boiled Free Range egg')
+streamlit.text (':star: Hard-boiled Free Range egg')
 streamlit.text('🥑🍞 Avocado Toast')
 
 streamlit.header('my own fruits')
 streamlit.text (':grapes:pears & guava')
 streamlit.text(':mango:')
 streamlit.text(':apple:')
+:star:
