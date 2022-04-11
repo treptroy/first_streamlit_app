c146d94352e5402c0b3b1b6b965a64b967f35c59
treptroy <103455753+treptroy@users.noreply.github.com>
2022-04-11 16:22:52 -0400

    Update streamlit_app.py
    bluberry - blueberry
    Hard_boiled - Hard-boiled

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,6 @@ import streamlit
 streamlit.title('My mom\'s New Healthy Diner')
 
 Breakfast Menu
-omega 3 & bluberry oatmeal
+omega 3 & blueberry oatmeal
 kale, Spinach & Rocket smoothie
-Hard_boiled Free Range egg
+Hard-boiled Free Range egg
