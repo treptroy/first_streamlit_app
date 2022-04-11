24905d1e9b3eb1b5ac8a5efe65ca0e2bbb169229
treptroy <103455753+treptroy@users.noreply.github.com>
2022-04-11 18:06:44 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,4 +6,5 @@ streamlit.header ('Breakfast Favorites')
 streamlit.text ('🥣omega 3 & blueberry oatmeal')
 streamlit.text ('🥗kale kale, Spinach & Rocket smoothie')
 streamlit.text ('🐔 Hard-boiled Free Range egg')
+streamlit.text('🥑🍞 Avocado Toast')
 
