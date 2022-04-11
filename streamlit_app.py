33903e30ab02dc8800981afa9396e81ead63fd62
treptroy <103455753+treptroy@users.noreply.github.com>
2022-04-11 19:38:18 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,6 +13,8 @@ streamlit.text (':grapes:pears & guava')
 streamlit.text(':mango:')
 streamlit.text(':apple:')
 
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
 import pandas
 
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
